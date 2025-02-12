04b5218eb7bfec637e08e30b1d9a27d1646f1556
sfaist <s.faistenauer@gmail.com>
2025-02-11 19:51:51 -0800

    fixed weirdness of schema generation

--- packages/core/utils/schema.ts
+++ packages/core/utils/schema.ts
@@ -1,27 +1,25 @@
 import { Validator } from "jsonschema";
 import OpenAI from "openai";
-import { z } from "zod";
-import { zodToJsonSchema } from "zod-to-json-schema";
+import { zodResponseFormat } from "openai/helpers/zod";
 import { GENERATE_SCHEMA_PROMPT } from "./prompts.js";
+import { z } from "zod";
 
-export async function generateSchema(instruction: string, responseData: string) : Promise<string> {
-
-  const schema = zodToJsonSchema(z.object({
-    jsonSchema: z.any()
-  }));
-  // call openai to create json schema based on instructino and response data
+export async function generateSchema(instruction: string, responseData: string, retry = 0, lastError?: string) : Promise<string> {
+  console.log("Generating schema");
+  
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 
   });
+
   const completion = await openai.chat.completions.create({
     model: process.env.OPENAI_MODEL,
     temperature: 0,
     response_format: {
       type: "json_schema",
       json_schema: {
-        name: "jsonSchema",
-        schema: schema,
+        name: "api_definition",
+        schema: { type: "object", properties: { jsonSchema: { type: "object" } } },
       }
     },
     messages: [
@@ -31,20 +29,15 @@ export async function generateSchema(instruction: string, responseData: string) 
       },
       {
         role: "user",
-        content: `Instruction: ${instruction}\n\nResponse Data: ${responseData}`
+        content: `Instruction: ${instruction}\n\nResponse Data: ${responseData}${lastError ? `\n\nLast Error: ${lastError}` : ''}`
       }
     ]
   });
-
-  
-  const generatedSchema = JSON.parse(completion.choices[0].message.content).properties.jsonSchema;
-  console.log(generatedSchema);
+    
+  const generatedSchema = JSON.parse(completion.choices[0].message.content).jsonSchema;
   // validate json schema
   const validator = new Validator();
   // unless a SchemaError is thrown, we are good
   const validation = validator.validate({}, generatedSchema);
-  if (!validation.valid || validation.errors.length > 0) {
-  }
-
   return generatedSchema;
 }
