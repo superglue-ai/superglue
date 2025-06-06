1c24ac248faa6b6e57360427f6e6f2fcf6cf2726
Michael Fuest <michaelfuest@Mac.fritz.box>
2025-06-06 18:25:30 +0200

    fixed providers issue

--- packages/web/src/app/providers.tsx
+++ packages/web/src/app/providers.tsx
@@ -1,21 +1,21 @@
 'use client'
+import posthog, { PostHog } from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import SuspendedPostHogPageView from '../components/utils/PHPageView'
 
-let posthog: any = undefined;
-
-export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
+export function CSPostHogProvider({ children }: { children: any }) {
   return (
     <PostHogProvider client={posthog}>
       <SuspendedPostHogPageView />
       {children}
     </PostHogProvider>
-  );
+  )
 }
+let postHog: PostHog | undefined = undefined;
 
 // Only in components
-if (typeof window !== 'undefined' && !posthog) {
-  posthog = posthog.init("phc_89mcVkZ9osPaFQwTp3oFA2595ne95OSNk47qnhqCCbE", {
+if (typeof window !== 'undefined' && !postHog) {
+  postHog = posthog.init("phc_89mcVkZ9osPaFQwTp3oFA2595ne95OSNk47qnhqCCbE", {
       ui_host: "https://us.posthog.com",
       api_host: 'https://d22ze2hfwgrlye.cloudfront.net',
       person_profiles: 'always',
