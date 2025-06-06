cd123c6d514ec0ac3fbf1ca9b0899060b103b3f9
Michael Fuest <michaelfuest@Mac.fritz.box>
2025-06-06 18:17:37 +0200

    rolled back unnecessary changes

--- packages/web/src/app/providers.tsx
+++ packages/web/src/app/providers.tsx
@@ -1,9 +1,10 @@
 'use client'
-import posthog, { PostHog } from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import SuspendedPostHogPageView from '../components/utils/PHPageView'
 
-export function Providers({ children }: { children: React.ReactNode }) {
+let posthog: any = undefined;
+
+export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return (
     <PostHogProvider client={posthog}>
       <SuspendedPostHogPageView />
@@ -12,11 +13,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   );
 }
 
-let postHog: PostHog | undefined = undefined;
-
 // Only in components
-if (typeof window !== 'undefined' && !postHog) {
-  postHog = posthog.init("phc_89mcVkZ9osPaFQwTp3oFA2595ne95OSNk47qnhqCCbE", {
+if (typeof window !== 'undefined' && !posthog) {
+  posthog = posthog.init("phc_89mcVkZ9osPaFQwTp3oFA2595ne95OSNk47qnhqCCbE", {
       ui_host: "https://us.posthog.com",
       api_host: 'https://d22ze2hfwgrlye.cloudfront.net',
       person_profiles: 'always',
