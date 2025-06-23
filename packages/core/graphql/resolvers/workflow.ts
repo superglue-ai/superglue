896d971394b72822adda5a9bc8563fb0eb0f1dba
Michael <michael@superglue.ai>
2025-06-23 10:02:19 +0200

    fixed ID generation bug

--- packages/core/graphql/resolvers/workflow.ts
+++ packages/core/graphql/resolvers/workflow.ts
@@ -260,7 +260,7 @@ export const buildWorkflowResolver = async (
     const builder = new WorkflowBuilder(instruction, resolvedIntegrations, payload, responseSchema, metadata);
     const workflow = await builder.build();
     // prevent collisions with existing workflows
-    workflow.id = await generateUniqueId(workflow.id, context.orgId, async (id, orgId) => !!(await context.datastore.getWorkflow(id, orgId)));
+    workflow.id = await generateUniqueId(workflow.id, async (id) => !!(await context.datastore.getWorkflow(id, context.orgId)));
 
     return workflow;
   } catch (error) {
