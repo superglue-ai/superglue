0aeb970ba1c3edc211852ea5b267efe1bdc3576c
Pallavi <pallavilpmt1995@gmail.com>
2025-07-30 21:34:51 +0530

    switch getRun graphql to REST

--- packages/core/restapi/runs/route.ts
+++ packages/core/restapi/runs/route.ts
@@ -1,11 +1,13 @@
 
 import { Router } from 'express';
-import { listRuns } from './controller.js'
+import { listRuns, getRun } from './controller.js'
 import { authMiddleware } from '../../auth/auth.js';
 
 const router = Router();
 
 router.get('/', authMiddleware, listRuns);
+
+router.get("/:id", authMiddleware, getRun);
 
 
 export default router;
