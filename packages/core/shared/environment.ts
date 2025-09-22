85b4ef41ef28459d2754e586d53d3766c50fb427
Nicolas Neudeck <nicolas@superglue.ai>
2025-09-22 14:36:30 +0200

    update turbo env variables and docker file npm installs

--- packages/core/shared/environment.ts
+++ packages/core/shared/environment.ts
@@ -14,15 +14,15 @@ export function validateEnvironment() {
     logMessage('warn', 'GRAPHQL_PORT is not set defaulting to 3000.');
   }
 
-  if ((process.env.LLM_PROVIDER !== 'OPENAI') && !process.env.OPENAI_API_KEY) {
+  if ((process.env.LLM_PROVIDER?.toUpperCase() !== 'OPENAI') && !process.env.OPENAI_API_KEY) {
     throw new Error('OPENAI_API_KEY is not set.');
   }
 
-  if ((process.env.LLM_PROVIDER === 'GEMINI') && !process.env.GEMINI_API_KEY) {
+  if ((process.env.LLM_PROVIDER?.toUpperCase() === 'GEMINI') && !process.env.GEMINI_API_KEY) {
     throw new Error('GEMINI_API_KEY is not set.');
   }
 
-  if ((process.env.LLM_PROVIDER === 'ANTHROPIC') && !process.env.ANTHROPIC_API_KEY) {
+  if ((process.env.LLM_PROVIDER?.toUpperCase() === 'ANTHROPIC') && !process.env.ANTHROPIC_API_KEY) {
     throw new Error('ANTHROPIC_API_KEY is not set.');
   }
 
