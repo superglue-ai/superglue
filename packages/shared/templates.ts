8b198d5061540977b13fa2a163c4cfbb6f08b4f9
Nicolas Neudeck <nicolas@superglue.ai>
2025-10-02 15:17:18 +0200

    airtable scopes update

--- packages/shared/templates.ts
+++ packages/shared/templates.ts
@@ -334,7 +334,7 @@ export interface IntegrationConfig {
       oauth: {
         authUrl: "https://airtable.com/oauth2/v1/authorize",
         tokenUrl: "https://airtable.com/oauth2/v1/token",
-        scopes: "data.records: read data. records: write data.recordComments: read data.recordComments: write schema.bases: read schema.bases: write webhook:manage user.email:read"
+        scopes: "data.records:read data.records:write data.recordComments:read data.recordComments:write schema.bases:read schema.bases:write webhook:manage user.email:read"
       },
       keywords: ["bases", "tables", "records", "fields", "views", "formulas", "attachments", "comments", "collaborators", "metadata", "schemas", "api key", "key"]
     },
