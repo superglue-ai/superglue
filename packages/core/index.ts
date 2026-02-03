a8acf59e13b6fe417a14ab6c9f67486f45a4058c
Nicolas Neudeck <nicolas@superglue.ai>
2026-02-03 17:49:29 +0100

    remove scheduler from index

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,8 +1,6 @@
 import { startApiServer } from "./api/api-server.js";
 import { createDataStore } from "./datastore/datastore.js";
 import { startGraphqlServer } from "./graphql/graphql-server.js";
-import { NotificationSummaryWorker } from "./scheduler/notification-summary-worker.js";
-import { ToolSchedulerWorker } from "./scheduler/scheduler-worker.js";
 import { validateEnvironment } from "./shared/environment.js";
 import { initializeWorkerPools } from "./worker/worker-pool-registry.js";
 
@@ -15,14 +13,6 @@ async function startServer() {
   });
 
   const workerPools = initializeWorkerPools(datastore);
-
-  if (process.env.START_SCHEDULER_SERVER === "true") {
-    const toolScheduler = new ToolSchedulerWorker(datastore, workerPools);
-    toolScheduler.start();
-
-    const notificationSummaryWorker = new NotificationSummaryWorker(datastore);
-    notificationSummaryWorker.start();
-  }
 
   await Promise.all([
     startApiServer(datastore, workerPools),
